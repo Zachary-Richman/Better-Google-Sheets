7a3b270c82a7698a3c79d565676e719cf65168a6
zjric <richmanzj@gmail.com>
2024-08-23 12:45:30 -0400

    base

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ reading = Reading('1g26kdbYZs_1Uvx862dZ37bK1Xa1u62HaFqpIzsdy9xI')
 generation = Generation('1g26kdbYZs_1Uvx862dZ37bK1Xa1u62HaFqpIzsdy9xI')
 deletion = Deletion('1g26kdbYZs_1Uvx862dZ37bK1Xa1u62HaFqpIzsdy9xI')
 
-deletion.delete_worksheet('My Worksheet!')
+print(reading.get_by_keyword(['English', 'Math'], 'Sample Data'))
--- utils/reading.py
+++ utils/reading.py
@@ -23,19 +23,43 @@ class Reading:
             if sheet['properties']['title'] == sheetname:
                 return sheet['properties']['sheetId']
 
-    def read_sheet(self, sheetname=None, sheet_id=None, range_=None):
-        if sheet_id and sheetname is None:
-            raise ValueError('You must specify a sheet name or ID.')
-        elif sheet_id and sheet_id is not None:
-            raise ValueError('You must cannot specify a sheet name and an ID.')
-
+    def read_sheet(self, sheet_id, range_=None):
         credentials = service_account.Credentials.from_service_account_file('service-account.json')
         service = discovery.build('sheets', 'v4', credentials=credentials)
 
-        if sheetname:
-            sheet_id = sheetname
         if range_ is None:
             range_ = sheet_id
 
         request = service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range_)
         return request.execute()
+
+    def read_row(self, row_num: int, sheet_id) -> list:
+        worksheet = self.read_sheet(sheet_id)['values']
+        if len(worksheet) > row_num:
+            return worksheet[row_num]
+        raise ValueError('Row num provided exceeds provided worksheet range.')
+
+    def tables_to_dict(self, key_column, sheet_id) -> dict:
+        worksheet = self.read_sheet(sheet_id)['values'][1:]
+
+        temp_: dict = {}
+        for row in worksheet:
+            key = row[key_column]
+            row_data = row[:key_column] + row[key_column + 1:]
+            temp_[key] = row_data
+        print(temp_)
+
+    def get_by_keyword(self, keyword: str or list, sheet_id):
+        worksheet = self.read_sheet(sheet_id)['values'][1:]
+
+        returned_data = []
+        if isinstance(keyword, list):
+            for row in worksheet:
+                if any(kw in row for kw in keyword):
+                    returned_data.append(row)
+        else:
+            for row in worksheet:
+                if keyword in row:
+                    returned_data.append(row)
+
+        return returned_data
